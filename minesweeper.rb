3f3f5bc6f66562bc467f45cb3b7b482115d149dc
App Academy Student <student@appacademy.io>
2016-03-10 17:47:14 -0800

    playable game with neighbors

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,11 +1,15 @@
 require 'byebug'
 
 class Tile
-  attr_accessor :face_up
-  def initialize(board)
+  attr_accessor :face_up, :flag, :row, :col, :value
+  def initialize(board,row,col)
     @board = board
     @bomb = false
     @face_up = false
+    @flag = false
+    @row = row
+    @col = col
+    @value = 0
   end
 
   def set_bomb
@@ -18,6 +22,22 @@ class Tile
 
   def reveal
     @face_up = true
+  end
+
+  def neighbors
+    (-1..1).to_a.each do |row_offset|
+      (-1..1).to_a.each do |col_offset|
+        next if row_offset == 0 && col_offset == 0
+        new_row = @row + row_offset
+        new_col = @col + col_offset
+        next if new_row < 0 || new_row > @board.grid.length-1
+        next if new_col < 0 || new_col > @board.grid[0].length-1
+        tile = @board.grid[new_row][new_col]
+        if tile.bomb?
+          @value += 1
+        end
+      end
+    end
   end
 end
 
@@ -32,11 +52,12 @@ class Board
   end
 
   def populate
-    grid.each do |row|
-      row.map! do |space|
-        space = Tile.new(self)
+    grid.each_with_index do |row, i|
+      row.map!.with_index do |space, j|
+        space = Tile.new(self,i,j)
       end
     end
+
   end
 
   def [](row, col)
@@ -50,8 +71,10 @@ class Board
   def render
     grid.each do |row|
       row.each do |tile|
-        if tile.face_up
-          (tile.bomb?) ? (print " X ") : (print " O ")
+        if tile.flag
+          print " F "
+        elsif tile.face_up
+          (tile.bomb?) ? (print " X ") : (print " #{tile.value} ")
         else
           print " _ "
         end
@@ -61,10 +84,17 @@ class Board
     puts ""
   end
 
-  def handle_move(input)
+  def handle_move(input,flag)
     x,y = input
-    grid[x][y].reveal
-    @bomb_found = true if @grid[x][y].bomb?
+    tile = grid[x][y]
+    if flag
+      tile.flag = !tile.flag
+    else
+      unless tile.flag
+        tile.reveal
+        @bomb_found = true if @grid[x][y].bomb?
+      end
+    end
   end
 
 end
@@ -92,8 +122,17 @@ class Game
     end
   end
 
+  def set_up_neighbors
+    board.grid.each do |row|
+      row.each do |tile|
+        tile.neighbors
+      end
+    end
+  end
+
   def play
     set_up_bombs
+    set_up_neighbors
     until over?
       board.render
       play_turn
@@ -107,11 +146,18 @@ class Game
   end
 
   def play_turn
-    puts "What space do you want to check? x,y"
-    input = gets.chomp.split(",").map(&:to_i)
-    @board.handle_move(input)
+    flag = false
+    puts "What space do you want to check? x,y or f to flag"
+    input = gets.chomp
+    if input == "f"
+      puts "What space do you want to flag? x,y"
+      input = gets.chomp
+      flag = true
+    end
+    input = input.split(",").map(&:to_i)
+    @board.handle_move(input,flag)
   end
 
 end
-g = Game.new(4,5, 18)
+g = Game.new(4,5, 5)
 g.play
